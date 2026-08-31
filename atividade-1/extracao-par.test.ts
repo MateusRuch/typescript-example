276eda0e2957fff590d96f54381364e1812f426b
MateusRuch <mateusrlucas@gmail.com>
2026-08-30 21:01:12 -0300

    exercício extração do número par concluído

---
+++ atividade-1/extracao-par.test.ts
@@ -0,0 +1,7 @@
+import { lista_pares } from "./extracao-par";
+
+test ("Teste extração dos números pares",() => {
+    let resultado_teste = lista_pares([8, 3, 9, 5, 6, 12]);
+    let resultado_esperado = [8, 6, 12]
+    expect(resultado_teste).toEqual(resultado_esperado)
+})
---
+++ atividade-1/extracao-par.ts
@@ -0,0 +1,5 @@
+let lista_pares = (pares:number[]) => { 
+    return pares.filter(num => num % 2 === 0);
+}
+
+export {lista_pares}
